5317ec4dd3020224945acae5e31e845a384a1e53
nine-hundred <ninehundred@outlook.kr>
2025-10-15 00:03:18 +0900

    feat: insert SQL, character set

--- init.sql
+++ init.sql
@@ -1,3 +1,6 @@
+SET NAMES utf8mb4;
+SET CHARACTER SET utf8mb4;
+
 CREATE TABLE IF NOT EXISTS schedules (
     id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(200) NOT NULL,
@@ -6,4 +9,6 @@ CREATE TABLE IF NOT EXISTS schedules (
     end_datetime DATETIME,
     is_completed BOOLEAN DEFAULT FALSE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+INSERT INTO `schedule_db`.`schedules` (`title`, `description`, `start_datetime`, `end_datetime`, `is_completed`) VALUES ("풋살", "정기 풋살 모임", '20251015080000', '20251015090000', '1');
